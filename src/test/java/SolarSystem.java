5a058d18e99941febabd76ded64b20c18886cd49
cnnsahin <canansahinkoyuncu@Canans-Air-2.attlocal.net>
2024-02-17 16:28:50 -0500

    The basic working solar system

--- src/test/java/SolarSystem.java
+++ src/test/java/SolarSystem.java
@@ -5,7 +5,29 @@ public class SolarSystem {
         solar.Stars="100";
         solar.Moon="4";
         solar.Planets="2";
+
+        FeatureSun xros= new FeatureSun();
+        xros.Color="pink";
+        xros.temp="23243243243234k";
+        xros.Size="234243432432432424miles";
+
+
+
+        Planet1 frog=new Planet1();
+        frog.color="blue";
+        frog.temp="-123213124k";
+        frog.size="2km";
+
+        Planet2 gecko =new Planet2();
+        gecko.color="magenta";
+        gecko.temp="34343443434k";
+        gecko.size="342344342m";
+
+
     }
 
 
-}
+    }
+
+
+
